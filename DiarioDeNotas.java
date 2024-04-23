a1dc373f0b0b4a9517694a2e4ba054c6597f2969
RayySantoss <163216225+RayySantoss@users.noreply.github.com>
2024-04-23 23:45:39 +0000

    Criação de classes diário de notas

--- DiarioDeNotas.java
+++ DiarioDeNotas.java
@@ -4,23 +4,22 @@ public class DiarioDeNotas{
     public static void main (String[] args){
         Scanner sc = new Scanner(System.in);
         Usuario usuario = new Usuario();
-        usuario.setTipoUsuario("Professor");
-        System.out.println(usuario.getTipoUsuario());
+       
 
         System.out.println("Digite 'Aluno' se você for um aluno e 'Professor' se você for um professor");
-        String  = sc.nextLine();
-         Aluno aluno = new Aluno();
-         aluno.IdentificarAluno();
-         Professor professor = new Professor();
-         professor.IdentificarProfessor();
-            if(usuario.equalsIgnoreCase("Professor")) {
-                System.out.println("Digite o seu nome"); 
-                String nome = sc.nextLine();
-                System.out.println("Ola Professor " + nome + " Seja bem vindo ");
-            }else if(usuario.equalsIgnoreCase("Aluno")) {
-                System.out.println("Digite o seu nome"); 
-                String nome = sc.nextLine();
-                System.out.println("Ola Aluno " + nome + ".Seja bem vindo ");
+        String usuario1  = sc.nextLine();
+        
+        
+            if(usuario1.equalsIgnoreCase("Professor")) {
+                Professor professor = new Professor();
+                professor.IdentificarProfessor();
+                usuario.setTipoUsuario("Professor");
+                System.out.println("Verificando o tipo de usuário " + usuario.getTipoUsuario());
+            
+            }else if(usuario1.equalsIgnoreCase("Aluno")) {
+                Aluno aluno = new Aluno();
+                aluno.IdentificarAluno();
+               
             } else {
                 System.out.println("Erro:opção inválida");
             }
