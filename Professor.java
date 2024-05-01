4678576f801ac8306be7b558639329e8d2ff555e
RayySantoss <163216225+RayySantoss@users.noreply.github.com>
2024-05-01 00:34:55 +0000

    criação da classe professor

--- Professor.java
+++ Professor.java
@@ -1,22 +1,5 @@
 import java.util.Scanner;
-public class Professor{
-    
+public class Professor extends Usuario{
 
-    String nome;
-    String email;
-    int idade;
     
-    Scanner sc = new Scanner(System.in);
-
-public void IdentificarProfessor(){
-    System.out.println("Digite seu nome  Professor");
-    nome= sc.nextLine();
-    System.out.println("Ola Professor " + nome + " Seja bem vindo ");
-    
-    System.out.println("Digite seu email Professor");
-    email= sc.nextLine();
-
-    System.out.println("Digite sua idade Professor");
-    idade = sc.nextInt();
-}
 }
